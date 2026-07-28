6ccc4ecd286d246465ed1669dc499f8f83a91708
Cantonaaa <2025270148@mails.szu.edu.cn>
2026-07-28 15:13:22 +0800

    Fix main.c compile errors

--- ESP/main/main.c
+++ ESP/main/main.c
@@ -4,6 +4,7 @@
 #include "esp_log.h"
 #include "nvs_flash.h"
 #include "esp_wifi.h"
+#include "esp_netif.h"
 #include "esp_event.h"
 #include "ble_daikin.h"
 #include "daikin_web.h"
@@ -30,39 +31,22 @@ static void wifi_init(void)
     esp_wifi_connect();
 }
 
-static void discover_units(void)
+static void ble_task(void *arg)
 {
-    unit_count = 4;
-    strcpy(units[0].name, "Living Room");
-    units[0].id = 1;
-    units[0].on = false;
-    strcpy(units[1].name, "Master Bed");
-    units[1].id = 2;
-    units[1].on = false;
-    strcpy(units[2].name, "Bedroom 2");
-    units[2].id = 3;
-    units[2].on = false;
-    strcpy(units[3].name, "Study");
-    units[3].id = 4;
-    units[3].on = false;
-    ESP_LOGI(TAG, "Pre-configured %d units", unit_count);
+    while (1) {
+        if (!ble_daikin_is_connected()) {
+            ESP_LOGI(TAG, "Connecting BLE...");
+            ble_daikin_connect();
+        }
+        vTaskDelay(pdMS_TO_TICKS(30000));
+    }
 }
 
 void app_main(void)
 {
     nvs_flash_init();
     wifi_init();
-    discover_units();
     ble_daikin_init();
     daikin_web_init();
-
-    xTaskCreatePinnedToCore([](void*) {
-        while (1) {
-            if (!ble_daikin_is_connected()) {
-                ESP_LOGI(TAG, "Attempting BLE connection...");
-                ble_daikin_connect();
-            }
-            vTaskDelay(pdMS_TO_TICKS(30000));
-        }
-    }, "ble_task", 4096, NULL, 5, NULL, 1);
+    xTaskCreatePinnedToCore(ble_task, "ble", 4096, NULL, 5, NULL, 1);
 }
