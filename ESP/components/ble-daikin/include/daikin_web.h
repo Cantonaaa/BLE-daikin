37705a2426f7bccd3dc52cfca85ebc08fcf4ad03
Cantonaaa <2025270148@mails.szu.edu.cn>
2026-07-28 17:26:52 +0800

    Add AP+WiFi config

--- ESP/components/ble-daikin/include/daikin_web.h
+++ ESP/components/ble-daikin/include/daikin_web.h
@@ -3,3 +3,6 @@
 
 esp_err_t daikin_web_init(void);
 void daikin_web_load_nvs(void);
+void load_wifi_creds(char *ssid, char *pass);
+void save_wifi_and_restart(const char *ssid, const char *pass);
+void wifi_init(void);
