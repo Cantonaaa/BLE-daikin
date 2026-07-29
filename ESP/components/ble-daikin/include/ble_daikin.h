a93e582fd6883c6fea38de00d4ecd17b4e2909e1
Cantonaaa <2025270148@mails.szu.edu.cn>
2026-07-29 10:36:42 +0800

    Sync header with latest declarations

--- ESP/components/ble-daikin/include/ble_daikin.h
+++ ESP/components/ble-daikin/include/ble_daikin.h
@@ -4,21 +4,31 @@
 #include "esp_err.h"
 
 #define MAX_UNITS 8
+#define MAX_DEVICES 20
 
 typedef struct {
     uint8_t id;
     bool on;
     char name[32];
-    uint16_t timer_on;   // HHMM format, 0 = disabled
-    uint16_t timer_off;  // HHMM format, 0 = disabled
+    uint16_t timer_on;
+    uint16_t timer_off;
 } daikin_unit_t;
+
+typedef struct {
+    uint8_t addr[6];
+    char name[32];
+    int8_t rssi;
+} discovered_device_t;
 
 extern daikin_unit_t units[MAX_UNITS];
 extern int unit_count;
+extern discovered_device_t discovered_devices[MAX_DEVICES];
+extern int discovered_count;
 
 esp_err_t ble_daikin_init(void);
-esp_err_t ble_daikin_connect(void);
-esp_err_t ble_daikin_disconnect(void);
+esp_err_t ble_daikin_start_scan(void);
+esp_err_t ble_daikin_connect_to(int device_index);
 bool ble_daikin_is_connected(void);
+bool ble_daikin_is_scanning(void);
 esp_err_t ble_daikin_set_power(uint8_t unit_id, bool on);
 void ble_daikin_timer_check(void);
